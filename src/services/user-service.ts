8db7622c96e45a30493024fd06d12b6a6d94731b
Filipus <filip.zytka@gmail.com>
2024-08-07 21:34:32 +0200

    user api refactor

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,3 +1,4 @@
+import { TCollaborator } from "../pages/Collaborators";
 import { SERVER_URL_USERS } from "./constants";
 import { getCookie } from "./helpers";
 
@@ -17,10 +18,9 @@ export const RegisterUser = async (email: string, password: string) => {
     const responseBody = await response.json();
 
     if (response.status == 200) {
-      return { success: true, message2: responseBody };
+      return { success: true, message: responseBody };
     }
-
-    return { success: false, error: responseBody };
+    return { success: false, message: responseBody.message };
   } catch (error) {
     return {
       success: false,
@@ -42,20 +42,30 @@ export const LoginUser = async (email: string, password: string) => {
       }),
       credentials: "include",
     });
+
+    const responseBody = await response.json();
+
     if (response.status == 200) {
-      return true;
+      return {
+        success: true,
+        role: responseBody.role,
+        email: responseBody.email,
+      };
     }
 
-    return false;
+    return { success: false, message: responseBody.message };
   } catch (error) {
     console.log(`Error while logging user! Error: ${error}`);
 
-    return false;
+    return {
+      success: false,
+      message: error,
+    };
   }
 };
 
 export const AuthenticateUser = async () => {
-  if (!getCookie("active")) return false;
+  if (!getCookie("active")) return;
 
   try {
     const response = await fetch(`${SERVER_URL_USERS}/auth`, {
@@ -66,15 +76,21 @@ export const AuthenticateUser = async () => {
       credentials: "include",
     });
 
+    const responseBody = await response.json();
+
     if (response.status === 200) {
-      return true;
+      return {
+        success: true,
+        role: responseBody.role,
+        email: responseBody.email,
+      };
     }
 
-    return false;
+    return { success: false };
   } catch (error) {
     console.log(`Error while authenticating user! Error: ${error}`);
 
-    return false;
+    return { success: false };
   }
 };
 
@@ -89,11 +105,55 @@ export const LogOutUser = async () => {
     });
 
     if (response.status === 200) {
-      return true;
+      return { success: true };
     }
   } catch (error) {
     console.log(`Error while authenticating user! Error: ${error}`);
 
-    return false;
+    return { success: false };
+  }
+};
+
+export const GetCollaborators = async () => {
+  try {
+    const response = await fetch(`${SERVER_URL_USERS}/collaborators`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    if (response.status === 200) {
+      const responseJson = await response.json();
+
+      return { success: true, collaborators: responseJson.collaborators };
+    }
+  } catch (error) {
+    console.log(`Error while fetching collaborators! Error: ${error}`);
+
+    return { success: false };
+  }
+};
+
+export const DeleteUsers = async (collaborators: TCollaborator[]) => {
+  try {
+    const response = await fetch(`${SERVER_URL_USERS}/collaborator/remove`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(collaborators),
+      credentials: "include",
+    });
+
+    if (response.status === 200) {
+      return { success: true, message: `Users has been removed` };
+    }
+    return { success: false };
+  } catch (error) {
+    console.log(`Error while deleting collaborators! Error: ${error}`);
+
+    return { success: false };
   }
 };
