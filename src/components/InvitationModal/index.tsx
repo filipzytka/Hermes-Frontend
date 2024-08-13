f195d43ac70ad12022831cf3ecd7e55056690fc6
Filipus <filip.zytka@gmail.com>
2024-08-13 20:03:01 +0200

    lower modal padding

--- src/components/InvitationModal/index.tsx
+++ src/components/InvitationModal/index.tsx
@@ -47,7 +47,7 @@ const InvitationModal = ({ isShowing, onSend, onClose }: Props) => {
             className="fixed inset-0 flex justify-center items-center z-50"
           >
             <div className="relative mt-7 bg-white rounded-xl shadow-lg dark:bg-gray-800 dark:border-gray-700">
-              <div className="px-10 py-4 sm:px-14">
+              <div className="px-5 py-5">
                 <div className="absolute right-4 top-4">
                   <button onClick={onClose}>
                     <svg
@@ -69,48 +69,46 @@ const InvitationModal = ({ isShowing, onSend, onClose }: Props) => {
                 <div className="flex flex-col justify-center items-center text-center">
                   <FaAddressBook className="dark:text-white text-cyan-600 w-10 h-10" />
                 </div>
-                <div className="w-80">
-                  <h1 className="block text-2xl  text-gray-800 dark:text-white mt-2">
-                    {"Add a collaborator to Hermes"}
-                  </h1>
-                  <div className="mt-5">
-                    <form onSubmit={handleSubmit}>
-                      <div className="flex flex-col gap-y-4">
-                        <div>
-                          <label className="block text-sm font-semibold ml-1 mb-3 dark:text-white text-gray-800">
-                            {"Search by email"}
-                          </label>
-                          <div className="relative">
-                            <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none text-gray-400">
-                              <CiSearch />
-                            </span>
-                            <input
-                              autoComplete="on"
-                              placeholder="Find people"
-                              onChange={handleInputChange}
-                              type="email"
-                              className="py-1 pl-8 pr-4 block w-full border-2
+                <h1 className="block text-2xl  text-gray-800 dark:text-white mt-2">
+                  {"Add a collaborator to Hermes"}
+                </h1>
+                <div className="mt-5">
+                  <form onSubmit={handleSubmit}>
+                    <div className="flex flex-col gap-y-4">
+                      <div>
+                        <label className="block text-sm font-semibold ml-1 mb-3 dark:text-white text-gray-800">
+                          {"Search by email"}
+                        </label>
+                        <div className="relative">
+                          <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none text-gray-400">
+                            <CiSearch />
+                          </span>
+                          <input
+                            autoComplete="on"
+                            placeholder="Find people"
+                            onChange={handleInputChange}
+                            type="email"
+                            className="py-1 pl-8 pr-4 block w-full border-2
                              border-gray-500 rounded-md text-sm
                              focus:border-cyan-600
                              focus:ring-cyan-700 shadow-sm
                              outline-none dark:bg-gray-800 bg-slate-50 dark:text-white text-gray-800"
-                              required
-                            ></input>
-                          </div>
-                        </div>
-                        <div className="flex items-center justify-center">
-                          <button
-                            type="submit"
-                            className="py-1 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold
-                            bg-cyan-600 text-white hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2
-                            transition-all text-sm dark:focus:ring-offset-gray-800 max-w-20"
-                          >
-                            {"Send"}
-                          </button>
+                            required
+                          ></input>
                         </div>
                       </div>
-                    </form>
-                  </div>
+                      <div className="flex items-center justify-center">
+                        <button
+                          type="submit"
+                          className="py-1 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold
+                            bg-cyan-600 text-white hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2
+                            transition-all text-sm dark:focus:ring-offset-gray-800 max-w-20"
+                        >
+                          {"Send"}
+                        </button>
+                      </div>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
