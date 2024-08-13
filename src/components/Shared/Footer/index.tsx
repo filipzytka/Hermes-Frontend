aba3c83a1a7369c78f0ded5c9bf69eb2db976e7a
Filipus <filip.zytka@gmail.com>
2024-08-13 14:42:40 +0200

    resize footer icon links

--- src/components/Shared/Footer/index.tsx
+++ src/components/Shared/Footer/index.tsx
@@ -7,19 +7,19 @@ const Footer = () => {
     <footer className="flex dark:bg-black/20 min-h-16 justify-center items-center shadow-lg w-full shrink-0 overflow-hidden">
       <a href="https://discord.com/">
         <FaDiscord
-          size={30}
+          size={20}
           className="mx-5 text-cyan-600 dark:text-gray-200 hover:animate-pulse cursor-pointer"
         />
       </a>
       <a href="https://x.com/home">
         <FaXTwitter
-          size={30}
+          size={20}
           className="mx-5 text-cyan-600 dark:text-gray-200 hover:animate-pulse cursor-pointer"
         />
       </a>
       <a href="https://youtube.com" className="mx-5">
         <FaYoutube
-          size={30}
+          size={20}
           className="text-cyan-600 dark:text-gray-200 hover:animate-pulse cursor-pointer"
         />
       </a>
