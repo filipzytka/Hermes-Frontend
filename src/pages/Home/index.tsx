9f3200d7b5aef9c90ef2325a2ab2b9d58e43376e
Filipus <filip.zytka@gmail.com>
2024-08-13 15:12:49 +0200

    add Main page components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
 import Footer from "../../components/Shared/Footer";
 import NavigationBar from "../../components/Shared/NavigationBar";
+import FeaturesCards from "./FeaturesCards";
+import HeroBullets from "./HeroBullets";
 
 const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <NavigationBar />
+      <HeroBullets />
+      <FeaturesCards />
       <div className="flex-grow"></div>
       <Footer />
     </div>
