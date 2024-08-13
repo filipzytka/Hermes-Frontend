68837f94f4d326cc14248c68a4b58f972a0c2bc5
Filipus <filip.zytka@gmail.com>
2024-08-13 16:23:51 +0200

    fix navbar elements moving on hero page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,8 +7,12 @@ const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <NavigationBar />
-      <HeroBullets />
-      <FeaturesCards />
+      <section id="hero-bullets" className="mt-24">
+        <HeroBullets />
+      </section>
+      <section id="feature-cards" className="my-6">
+        <FeaturesCards />
+      </section>
       <div className="flex-grow"></div>
       <Footer />
     </div>
