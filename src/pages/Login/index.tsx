16e26feb1d646cd1e35d11fd8ff940b263f6cd9d
Filipus <filip.zytka@gmail.com>
2024-08-18 12:17:07 +0200

    remove unused imports

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../../api/user.ts";
 import { useAuth } from "../../hooks/useAuth";
