6b3d7a0074bcf229529e440fb9f7e743eea3a231
Filipus <filip.zytka@gmail.com>
2024-08-19 22:32:48 +0200

    reduce mt on mobile in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <NavigationBar />
-      <section id="hero-bullets" className="mt-32">
+      <section id="hero-bullets" className="mt-8 md:mt-32">
         <HeroBullets />
       </section>
       <section id="feature-cards" className="m-6">
