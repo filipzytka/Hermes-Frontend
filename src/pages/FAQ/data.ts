80846bb4b4194a9e841ab493e1adae79371d8ac7
Filipus <filip.zytka@gmail.com>
2024-08-20 13:10:40 +0200

    refactor faq page

---
+++ src/pages/FAQ/data.ts
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    value: "add-collaborators",
+    question: "How do I add collaborators via email?",
+    answer: `To add collaborators, go to the 'Collaborators' section, enter the
+              email address of the person you want to add, and select their
+              role. An invitation will be sent to their email.`,
+  },
+  {
+    value: "permissions",
+    question: "How can I manage permissions for different collaborators?",
+    answer: `Permissions can be managed by navigating to the 'Permissions' tab
+              under 'Settings'. Here, you can assign or revoke specific
+              permissions for each collaborator.`,
+  },
+  {
+    value: "view-charts",
+    question: `How can I view and customize the charts of statistics?`,
+    answer: `You can view and customize charts from the 'Analytics' section.
+              Choose the metrics you want to display and adjust the time range
+              or filters to tailor the data to your needs.`,
+  },
+  {
+    value: "notifications",
+    question: `Can I set up notifications for server resource thresholds?`,
+    answer: `Yes, go to the 'Notifications' settings to configure alerts. You
+              can set thresholds for different server resources, and choose how
+              you would like to be notified.`,
+  },
+  {
+    value: "monitor-resources",
+    question: `What tools are available for monitoring server resources?`,
+    answer: `Our platform provides a real-time dashboard with CPU, memory, and
+              disk usage stats. You can set up alerts for when resources reach
+              critical levels.`,
+  },
+];
--- src/pages/FAQ/index.tsx
+++ src/pages/FAQ/index.tsx
@@ -1,8 +1,17 @@
-import { Container, Title, Accordion } from "@mantine/core";
+import {
+  Container,
+  Title,
+  Accordion,
+  useMantineColorScheme,
+} from "@mantine/core";
 import NavigationBar from "../../components/Shared/NavigationBar";
 import Footer from "../../components/Shared/Footer";
+import { data } from "./data";
 
 const FAQ = () => {
+  const { colorScheme } = useMantineColorScheme();
+  const isDark = colorScheme === "dark";
+
   return (
     <div className="flex flex-col min-h-screen w-full">
       <NavigationBar />
@@ -15,60 +24,17 @@ const FAQ = () => {
         </Title>
 
         <Accordion variant="separated">
-          <Accordion.Item value="add-collaborators">
-            <Accordion.Control>
-              How do I add collaborators via email?
-            </Accordion.Control>
-            <Accordion.Panel>
-              To add collaborators, go to the 'Collaborators' section, enter the
-              email address of the person you want to add, and select their
-              role. An invitation will be sent to their email.
-            </Accordion.Panel>
-          </Accordion.Item>
-
-          <Accordion.Item value="permissions">
-            <Accordion.Control>
-              How can I manage permissions for different collaborators?
-            </Accordion.Control>
-            <Accordion.Panel>
-              Permissions can be managed by navigating to the 'Permissions' tab
-              under 'Settings'. Here, you can assign or revoke specific
-              permissions for each collaborator.
-            </Accordion.Panel>
-          </Accordion.Item>
-
-          <Accordion.Item value="view-charts">
-            <Accordion.Control>
-              How can I view and customize the charts of statistics?
-            </Accordion.Control>
-            <Accordion.Panel>
-              You can view and customize charts from the 'Analytics' section.
-              Choose the metrics you want to display and adjust the time range
-              or filters to tailor the data to your needs.
-            </Accordion.Panel>
-          </Accordion.Item>
-
-          <Accordion.Item value="monitor-resources">
-            <Accordion.Control>
-              What tools are available for monitoring server resources?
-            </Accordion.Control>
-            <Accordion.Panel>
-              Our platform provides a real-time dashboard with CPU, memory, and
-              disk usage stats. You can set up alerts for when resources reach
-              critical levels.
-            </Accordion.Panel>
-          </Accordion.Item>
-
-          <Accordion.Item value="notifications">
-            <Accordion.Control>
-              Can I set up notifications for server resource thresholds?
-            </Accordion.Control>
-            <Accordion.Panel>
-              Yes, go to the 'Notifications' settings to configure alerts. You
-              can set thresholds for different server resources, and choose how
-              you would like to be notified.
-            </Accordion.Panel>
-          </Accordion.Item>
+          {data.map((d) => (
+            <Accordion.Item
+              value={d.value}
+              style={{
+                backgroundColor: isDark ? "#0e141f" : undefined,
+              }}
+            >
+              <Accordion.Control>{d.question}</Accordion.Control>
+              <Accordion.Panel>{d.answer}</Accordion.Panel>
+            </Accordion.Item>
+          ))}
         </Accordion>
       </Container>
       <Footer />
