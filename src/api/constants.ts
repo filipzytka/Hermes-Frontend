15bf676c833f3017193fae62169200fef491b514
Filipus <filip.zytka@gmail.com>
2024-08-21 21:36:24 +0200

    banlist url constant

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,5 +1,6 @@
 export const SERVER_URL_USERS = `${import.meta.env.VITE_SERVER_URL}/api/users`;
 export const SERVER_URL_EMAIL = `${import.meta.env.VITE_SERVER_URL}/api/email`;
 export const SERVER_URL_TOKEN = `${import.meta.env.VITE_SERVER_URL}/api/invite`;
+export const SERVER_URL_BANLIST = `${import.meta.env.VITE_SERVER_URL}/api/ban`;
 
 export const REACT_URL = `${import.meta.env.VITE_CLIENT_URL}`;
