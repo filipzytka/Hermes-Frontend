c9ca198229858393190b18845cf08b7cdfc6bccf
Filipus <filip.zytka@gmail.com>
2024-08-21 23:00:20 +0200

    responsive fix

--- src/pages/Collaborators/index.tsx
+++ src/pages/Collaborators/index.tsx
@@ -66,9 +66,9 @@ const Collaborators = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <NavigationBar />
-      <div className="flex-grow flex flex-col items-center px-3 mt-8 md:mt-32">
+      <div className="flex-grow flex flex-col md:items-center px-3 mt-8 md:mt-32">
         <div className="flex justify-start"></div>
-        <div className="min-w-96 md:w-1/2 flex flex-col">
+        <div className="md:w-1/2 flex flex-col">
           <div className="flex-grow">
             <DataTable
               onDelete={handleCollaboratorRemoval}
