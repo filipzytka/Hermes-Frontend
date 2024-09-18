ad30c7818576c88cfc80cfa0e0cbe9f7b57cafa4
Filipus <filip.zytka@gmail.com>
2024-09-18 19:12:06 +0200

    remove testing herobullets test

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,28 +1,4 @@
 describe("main page", () => {
-  it("should show HeroBullets when entering home page", () => {
-    cy.visit(`${Cypress.env("LOCALHOST")}/`);
-    cy.get('[data-cy="hero-bullets"]').should("exist");
-  });
-
-  it("should show info when clicking faq sections", () => {
-    cy.visit(`${Cypress.env("LOCALHOST")}/faq`);
-    cy.get('[data-cy="faq"]').should("exist");
-    cy.get('[data-cy="faq-item-0"').should("exist").click();
-  });
-
-  it("should navigate to given pages when clicking navbar links", () => {
-    cy.visit(`${Cypress.env("LOCALHOST")}`);
-
-    cy.get('[data-cy="link-home"]').should("exist").click();
-    cy.url().should("match", /\/$/);
-
-    cy.get('[data-cy="link-faq"]').should("exist").click();
-    cy.url().should("match", /\/faq$/);
-
-    cy.get('[data-cy="link-signin"]').should("exist").click();
-    cy.url().should("match", /\/login$/);
-  });
-
   it("should dashboard link be not visible when not signed in", () => {
     cy.visit(`${Cypress.env("LOCALHOST")}`);
 
