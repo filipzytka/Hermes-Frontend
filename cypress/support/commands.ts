98d39fc3d4fabafb3cc39d7871425bbdff0d620d
Filipus <filip.zytka@gmail.com>
2024-09-25 02:57:05 +0200

    w

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 Cypress.Commands.add("loginByApi", (email: string, password: string) => {
-  return cy.request("POST", `${Cypress.env("SERVER_URL")}/api/auth/login`, {
+  return cy.request("POST", `http://localhost:8080/api/auth/login`, {
     email,
     password,
   });
@@ -10,7 +10,7 @@ Cypress.Commands.add("loginByApi", (email: string, password: string) => {
 Cypress.Commands.add("removeTestUser", (email: string, role: string) => {
   return cy.request({
     method: "DELETE",
-    url: `${Cypress.env("SERVER_URL")}/api/users/collaborator/remove`,
+    url: `http://localhost:8080}/api/users/collaborator/remove`,
     body: [
       {
         email,
