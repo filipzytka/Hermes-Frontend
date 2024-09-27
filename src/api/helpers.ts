83ea1e0aadad07c0fb25574e2926f1b15c5ae792
Filipus <filip.zytka@gmail.com>
2024-09-27 18:14:44 +0200

    remove get cookie

--- src/api/helpers.ts
+++
@@ -1,9 +0,0 @@
-export const getCookie = (name: string): string | undefined => {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) {
-    const cookieValue = parts.pop()?.split(";").shift();
-    return cookieValue;
-  }
-  return undefined;
-};
